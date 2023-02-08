97472b8356c7ff05f557e48883915b5c213d30b3
Henok100 <henokgashaw15@gmail.com>
2023-02-08 07:57:39 +0300

    Yes

--- Exampe2/SocketServer.cpp
+++ Exampe2/SocketServer.cpp
@@ -12,6 +12,7 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
+#define PORT 9909
 
 using namespace std;
 
@@ -19,8 +20,8 @@ int main()
 {
     int nSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    struct sockaddr_in server_addr;
-    socklen_t size;
+    struct sockaddr_in srv; //Server address
+    fd_set fr, fw, fe;
 
     /* ---------- ESTABLISHING SOCKET CONNECTION ----------*/
     /* --------------- socket() function ------------------*/
@@ -31,17 +32,39 @@ int main()
         exit(1);
     }
 
+    cout << "\n=> Socket server has been created..." << nSocket <<endl;
 
-    cout << "\n=> Socket server has been created..." << endl;
-
-
-    /*server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htons(INADDR_ANY);
-    server_addr.sin_port = htons(portNum);
-
+    srv.sin_family = AF_INET;
+    srv.sin_addr.s_addr = htons(INADDR_ANY);
+    srv.sin_port = htons(PORT); // host to network byte order
+    memset(&(srv.sin_zero), 0, 8);
 
     /* ---------- BINDING THE SOCKET ---------- */
     /* ---------------- bind() ---------------- */
+
+    int nRet = 0;
+
+    nRet = bind(nSocket, (sockaddr*)&srv, sizeof(sockaddr));
+    if (nRet < 0){
+        cout << "Failed at binding" << endl;
+        exit(1);
+    }
+    else {
+        cout << "Successfully bind to local port" << endl;
+    }
+
+    ////////////////// Listen //////////////////////
+    nRet = listen(nSocket, 5);
+
+    if (nRet < 0){
+        cout << "Failed at listen to local port" << endl;
+        exit(1);
+    }
+    else {
+        cout << endl << "Started listening to local port" << endl;
+    }
+    ///////////////////////////////////////////////
+
 
 
     return 0;
