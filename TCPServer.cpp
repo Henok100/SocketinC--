d1be795a391fb223d4b09a9a5cca1fe8385af761
Henok100 <henokgashaw15@gmail.com>
2023-02-18 22:02:32 +0300

    Cool

--- TCPServer.cpp
+++ TCPServer.cpp
@@ -39,10 +39,73 @@ int main()
     }
 
     // Accept a call
-    
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+    char host[NI_MAXHOST];
+    char service[NI_MAXSERV];
+
+    int clientSocket = accept(listening, 
+                              (sockaddr*) &client,
+                               &clientSize);
+
+    if (clientSocket == -1)
+    {
+        cerr << "Problem with client connecting";
+        return -4;
+    }
+
+
     // Close a listening socket
+    close(listening);
+
+    memset(host, 0, NI_MAXHOST);
+    memset(service, 0, NI_MAXSERV);
+
+    int result = getnameinfo((sockaddr*)&client,
+                              sizeof(client),
+                              host,
+                              NI_MAXHOST,
+                              service,
+                              NI_MAXSERV,
+                              0);
+    if (result)
+    {
+        cout << host << " connected on " << service << endl;
+    }
+    else
+    {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        cout << host << " connected on " << ntohs(client.sin_port) << endl;
+    }
+
     // display and echo
+    char buf[4096];
+    while (true)
+    {
+        // Clear the buffer
+        memset(buf, 0, 4096);
+        // Wait for message
+        int bytesRecv = recv(clientSocket, buf, 4096, 0);
+        if (bytesRecv == -1)
+        {
+            cerr << "There is a connection proble.\n";
+            break;
+        }
+        if (bytesRecv == 0)
+        {
+            cerr << "The client disconnected.\n";
+            break;
+        }
+        // display message
+        cout << "Received: " << string(buf, 0, bytesRecv) << endl;
+        
+        // Resend message
+        send(clientSocket, buf, bytesRecv + 1, 0);
+        
+    }
+
     // Close
+    close (clientSocket); 
 
     return 0;
 }
